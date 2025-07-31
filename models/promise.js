8f25ad7d85962b8de34a7ad3a5cc50d17560a9e1
mirellesoares <mirelle.soares4@aluno.enova.educacao.ba.gov.br>
2025-07-31 17:25:49 +0000

    ok

--- models/promise.js
+++ models/promise.js
@@ -4,6 +4,20 @@ const terminal = readFile.createInterface({
   output: process.stdout,
 });
 
-terminal.question("Qual seu nome: /n", function (valor){
-  console.log("seja bem vindo " + valor);
+// terminal.question("Qual seu nome: /n", function (valor){
+//   console.log("seja bem vindo " + valor);
+// })
+
+function question(pergunta){
+  return new Promise(function(resolve, reject) {
+    terminal.question(pergunta + "\n", function(valor) {
+      resolve(valor);
+    });
+  });
+}
+question("Qual seu nome").then(function(valor) {
+  console.log(`Seja bem-vindo ${valor}`);
 })
+.finally(function() {
+  terminal.close();
+});
