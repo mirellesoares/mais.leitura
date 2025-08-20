ecfa4e6cd55de63ded434092421e6e7993dac76d
mirellesoares <mirelle.soares4@aluno.enova.educacao.ba.gov.br>
2025-08-20 17:26:29 +0000

    ok

--- models/promise.js
+++ models/promise.js
@@ -9,15 +9,23 @@ const terminal = readFile.createInterface({
 // })
 
 function question(pergunta){
-  return new Promise(function(resolve, reject) {
-    terminal.question(pergunta + "\n", function(valor) {
+  const promise = new Promise(function(resolve, reject) {
+    terminal.question(pergunta, function(valor) {
       resolve(valor);
     });
   });
+  return promise;
 }
-question("Qual seu nome").then(function(valor) {
-  console.log(`Seja bem-vindo ${valor}`);
+question("Qual seu nome? \n")
+.then(function(nome) {
+  console.log("nome: " + nome);
+})
+.catch(function(erro) {
+  console.log("Deu erro: " + erro);
 })
 .finally(function() {
   terminal.close();
 });
+// terminal.question("Qual seu nome: /n", function (valor){
+//   console.log("seja bem vindo " + valor);
+// })
