e3c25c139c1d28c74a54c8d6e248f8e74d55cb2b
mirellesoares <mirelle.soares4@aluno.enova.educacao.ba.gov.br>
2025-10-08 17:23:09 +0000

    salvando

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
+require("dotenv").config();
+console.log(process.env);
 const {query} = require("./infra/database");
 
-console.log(process.env.NAME);
+console.log(process.env.DB_NAME);
+console.log(process.env.DB_HOST);
+console.log(process.env.DB_PORT);
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASSWORD);
 
-
-//query("SELECT 1+1;")
-// .then(function(value) {
-//   console.log(value);
-// })
-// .catch(function(err) {
-//   console.log("Deu ruim", err);
-//});
+query("SELECT 1+1;")
+.then(function(value) {
+  console.log(value);
+})
+.catch(function(err) {
+  console.log("Deu ruim", err);
+});
