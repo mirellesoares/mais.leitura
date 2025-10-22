d6ae06d2d0b26071a97cb636af7c79c67258d1cd
mirellesoares <mirelle.soares4@aluno.enova.educacao.ba.gov.br>
2025-10-22 17:16:33 +0000

    ok

---
+++ infra/migrations/1761151138129_criar-tabela-livro.js
@@ -0,0 +1,41 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+
+  pgm.sql(`
+    CREATE TABLE livro (
+    id VARCHAR(36) PRIMARY KEY,
+    numero_paginas INTEGER,
+    autor VARCHAR(255),
+    titulo VARCHAR(100) NOT NULL,
+    genero VARCHAR(100) NOT NULL,
+    data_publicacao DATE,
+    isbn TEXT,
+    resumo TEXT,
+    imagem_capa TEXT,
+    editora VARCHAR(225),
+    disponibilidade BOOLEAN DEFAULT true,
+    tags TEXT,
+    preco NUMERIC(10, 2) DEFAULT 0.0
+    );
+    `)
+};
+
+  pgm.sql(`
+    DROP TABLE livro;
+    `)
