193174af2a1534472c05a4d2b0b21c0338f45089
mirellesoares <mirelle.soares4@aluno.enova.educacao.ba.gov.br>
2025-11-05 16:47:01 +0000

    ok

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 require("dotenv").config();
+const { port } = require("pg/lib/defaults");
 const livroRepository = require("./infra/repository/livro");
-livroRepository
-.consultarTodos()
-.then(function(resultado) {
-    console.log(resultado);
-})
-.catch(function(error) {
-    console.error("Deu ruim", erro);
+const http = require("node:http");
+
+const server = http.createServer(function (req, res) {
+  res.end("ok bem vindo");
 });
-  
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(port,function() {
+  console.log("inicializando servidor HTTP na porta " + port);
+});
